36208669345732eb1fe3590786ebcae47eed2988
Umberto Palo <u.palo@U-PALO.replynet.prv>
2017-11-08 15:11:47 +0100

    primo commit

---
+++ demoSpringBootMVC/src/main/java/it/umberto/palo/DemoSpringBootMvcApplication.java
@@ -0,0 +1,12 @@
+package it.umberto.palo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootMvcApplication.class, args);
+	}
+}
